a07da902cbace9e64874c1247ecf7bef2ae12b61
Viveksah7875 <119647536+Viveksah7875@users.noreply.github.com>
2025-06-24 22:30:21 +0530

    Time: 15 ms (10.84%), Space: 22 MB (14.6%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -13,13 +13,19 @@ public:
         // }
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        int i=0;
-        while(i<n)
-        {
-            if(nums[i]!=i) return i;
-            i++;
+        int low=0;int high=n-1;
+        
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==mid){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
-       return n;
+        return low ;
+
      }
                
 
