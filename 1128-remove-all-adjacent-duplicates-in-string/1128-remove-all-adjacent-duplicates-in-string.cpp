6b9d36321b0cae4190612b89ab075105a4e46c6d
Viveksah7875 <119647536+Viveksah7875@users.noreply.github.com>
2025-06-26 13:22:22 +0530

    Time: 2 ms (85.45%), Space: 13.6 MB (76.9%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -3,13 +3,15 @@ public:
     string removeDuplicates(string s) {
         string ans;
         int n = s.size();
-        for (int i = 0; i < n; i++) {
-            char current_char = s[i];
+        // for (int i = 0; i < n; i++) {
+        //     char current_char = s[i];
+        for(char ch:s)
+        {
 
-            if (!ans.empty() && current_char == ans.back())
+            if (!ans.empty() && ch == ans.back())
                 ans.pop_back();
             else {
-                ans.push_back(current_char);
+                ans.push_back(ch);
             }
         }
         return ans;
