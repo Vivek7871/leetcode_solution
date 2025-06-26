40533167c4ebab50cb8ce6d9737bca1bb6f2d40f
Viveksah7875 <119647536+Viveksah7875@users.noreply.github.com>
2025-06-26 13:21:27 +0530

    Time: 4 ms (70.78%), Space: 13.7 MB (51.6%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,36 +1,17 @@
-// class Solution {
-// public:
-//     string removeDuplicates(string s) {
-//        vector<int>ans;
-//        int n=s.size();
-//        int i=0;
-//        int j=1;
-//        while(j<n){
-//         if(s[i]!=s[j]){
-//             ans.push_back();
-//             i++; j++;
-//         }
-//         else{
-//             ans.pop_back();
-//         }
-//        }
-//        return ans;
-       
-//     }
-// };
 class Solution {
 public:
     string removeDuplicates(string s) {
         string ans;
-        int n=s.size();
-      for(int i=0;i<n;i++){
-        char current_char=s[i];
-        // if(ans.empty()) ans.push_back(current_char);
-        if(!ans.empty()&&current_char==ans.back()) ans.pop_back();
-        else{
-            ans.push_back(current_char);
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            char current_char = s[i];
+
+            if (!ans.empty() && current_char == ans.back())
+                ans.pop_back();
+            else {
+                ans.push_back(current_char);
+            }
         }
-      }
-      return ans;
+        return ans;
     }
 };
