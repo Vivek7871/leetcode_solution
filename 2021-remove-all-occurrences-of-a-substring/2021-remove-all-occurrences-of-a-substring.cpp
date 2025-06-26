1046d05a7954348eb31ed9ef9f8fa2bdbb2a8252
Viveksah7875 <119647536+Viveksah7875@users.noreply.github.com>
2025-06-26 23:33:06 +0530

    Time: 0 ms (100%), Space: 9.4 MB (69.78%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,11 +1,40 @@
+// class Solution {
+// public:
+  
+//     string removeOccurrences(string s, string part) {
+        
+//         while(s.find(part)!=string::npos){
+//             s.erase(s.find(part),part.length());
+//         }
+//         return s;
+//     }
+// };
 class Solution {
 public:
-  
     string removeOccurrences(string s, string part) {
-        
-        while(s.find(part)!=string::npos){
-            s.erase(s.find(part),part.length());
+        string res = "";
+        int m = part.length();
+
+        for (char ch : s) {
+            res.push_back(ch);  // Step 1
+
+            if (res.size() >= m) {  // Step 2
+                bool match = true;
+
+                for (int i = 0; i < m; ++i) {  // Step 3
+                    if (res[res.size() - m + i] != part[i]) {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match) {  // Step 4
+                    for (int i = 0; i < m; ++i)
+                        res.pop_back();
+                }
+            }
         }
-        return s;
+
+        return res;
     }
 };
