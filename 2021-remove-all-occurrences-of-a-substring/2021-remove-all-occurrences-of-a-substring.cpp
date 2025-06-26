dbcfe77da059ab57b765cc955ab24d37f56092db
Viveksah7875 <119647536+Viveksah7875@users.noreply.github.com>
2025-06-26 23:47:28 +0530

    Time: 0 ms (100%), Space: 9.5 MB (18.95%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,8 +1,8 @@
 // class Solution {
 // public:
-  
+
 //     string removeOccurrences(string s, string part) {
-        
+
 //         while(s.find(part)!=string::npos){
 //             s.erase(s.find(part),part.length());
 //         }
@@ -16,19 +16,24 @@ public:
         int m = part.length();
 
         for (char ch : s) {
-            res.push_back(ch);  // Step 1
+            res.push_back(ch); // res me ch ki balue push karo
 
-            if (res.size() >= m) {  // Step 2
+            if (res.size() >=m) { // jab tak res ki value m ya m se badi nhi ho jaati tab tak
+                     // res me value push krte jao agr jaise hi res ki m k equal
+                     // ya bda ho jaye usko compare krwa do part k sath.
                 bool match = true;
 
-                for (int i = 0; i < m; ++i) {  // Step 3
-                    if (res[res.size() - m + i] != part[i]) {
+                for (int i = 0; i < m;++i) { // iss line hum compare kr rhe hai res ki value ko
+                            // part k value k sath
+                    if (res[res.size() - m + i] !=part[i]) { // ye line me me hum last 3 value of res ko
+                                   // ek ek kar k part k value k sath compare
+                                   // kar he hai .
                         match = false;
                         break;
                     }
                 }
 
-                if (match) {  // Step 4
+                if (match) { // agr match true rehha hai to hum res ka last 3 value jo part se match hua hai usko pop out kr lete hai
                     for (int i = 0; i < m; ++i)
                         res.pop_back();
                 }
