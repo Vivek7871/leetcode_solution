01b3a74349ec4931a9171d8c338ddef58f42044c
Vivek2120 <119647536+Vivek7871@users.noreply.github.com>
2025-07-03 23:08:21 +0530

    Time: 5 ms (35.67%), Space: 18 MB (55.87%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
-        vector<int> minutes;
-
-        //  Convert HH:MM to minutes
-        for (string time : timePoints) {
-            int hour = stoi(time.substr(0, 2));
-            int minute = stoi(time.substr(3, 2));
-            minutes.push_back(hour * 60 + minute);
+        vector <int> minutes;
+        int i=0;
+        while(i<timePoints.size()){
+            int hours = stoi(timePoints[i].substr(0,2));
+            int min= stoi(timePoints[i].substr(3,2));
+            minutes.push_back(hours*60+min);
+            i++;
         }
+        sort(minutes.begin(),minutes.end());
+        int min_diff= INT_MAX;
+        for(int i=1;i<minutes.size();i++){
+            min_diff=min(min_diff,minutes[i]-minutes[i-1]);
 
-        // Sort the minutes
-        sort(minutes.begin(), minutes.end());
-
-        int minDiff = INT_MAX;
-
-        //  Check difference between adjacent times
-        for (int i = 1; i < minutes.size(); ++i) {
-            minDiff = min(minDiff, minutes[i] - minutes[i - 1]);
         }
+        int circular_diff = (1440 - minutes.back() + minutes[0]);
+        min_diff = min(min_diff, circular_diff);
 
-        //  Check difference between last and first time 
-        int lastFirstDiff = 1440 - minutes.back() + minutes[0];
-        minDiff = min(minDiff, lastFirstDiff);
-
-        return minDiff;
+        return min_diff;
+        
     }
 };
