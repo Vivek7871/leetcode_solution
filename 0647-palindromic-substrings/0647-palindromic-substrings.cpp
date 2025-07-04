e7d97e8d175e32c79aeb087755a3f354a6cfb697
Vivek2120 <119647536+Vivek7871@users.noreply.github.com>
2025-07-04 17:18:04 +0530

    Time: 1098 ms (11.26%), Space: 464.2 MB (5.03%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,18 +1,63 @@
+// class Solution {
+// public:
+//     int countSubstrings(string s) {
+//         vector<char> ans;
+//         vector<string> ans2;
+//         int n = s.size();
+//         for (int i = 0; i < n; i++) {
+//             ans.push_back(s[i]);
+//         }
+//         string temp = "";
+//         for (int i = 0; i < n-1; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (s[i] == s[j]) {
+                    
+//                     temp += s[i];
+//                     temp += s[j];
+//                     ans2.push_back(temp);
+//                     temp.clear();
+
+//                 } else {
+//                     j++;
+//                 }
+//             }
+//         }
+//         return ans.size()+ans2.size();
+
+//     }
+// };
+
 class Solution {
 public:
-    int countSubstrings(string s) {
-        int n = s.size();
-        int count = 0;
-        for (int center = 0; center < 2 * n - 1; center++) {
-            int left = center / 2;
-            int right = left + center % 2;
+    // Helper function to check if a substring is palindrome
+    bool isPalindrome(string str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str[i] != str[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
 
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                count++;
-                left--;
-                right++;
+    int countSubstrings(string s) {
+        int count = 0;
+        int n = s.size();
+
+        // Traverse all possible substrings
+        for (int i = 0; i < n; i++) {
+            string temp = "";
+            for (int j = i; j < n; j++) {
+                temp += s[j]; // form substring from i to j
+                if (isPalindrome(temp)) {
+                    count++;
+                }
             }
         }
+
         return count;
     }
 };
+
+  
