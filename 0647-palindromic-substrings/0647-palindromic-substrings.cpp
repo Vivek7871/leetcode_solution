9a156eba7165fbc39aaa4b68c1c702c37e227ab9
Vivek2120 <119647536+Vivek7871@users.noreply.github.com>
2025-07-04 18:30:32 +0530

    Time: 10 ms (57.99%), Space: 21 MB (36.64%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,27 +1,30 @@
-// leetcode647. Palindromic Substrings
 class Solution {
 public:
-    int expandAroundCenter(string s, int i, int j){
+    int expandAroundCenter(string s, int i, int j) {
         int count = 0;
-        while(i>=0 && j<=s.length() && s[i]==s[j]){ //i and j should be valid index
-            count++;
-            i--;
-            j++;
+        while(i >= 0 && j < s.length() && s[i] == s[j]) 
+        {
+
+                count++;
+                i--;
+                j++;
         }
         return count;
     }
     int countSubstrings(string s) {
+
         int totalCount = 0;
-        for(int center = 0; center < s.length(); center++){
-            //odd length substr
+        for (int center = 0; center < s.length(); center++) {
+            // odd
             int i = center;
             int j = center;
-            int oddPalindromeSubstrCount = expandAroundCenter(s, i ,j);
-            //even length substr
+            int oddPalSubStringKaCount = expandAroundCenter(s, i, j);
+            // even
             i = center;
             j = center + 1;
-            int evenPalindromeSubstrCount = expandAroundCenter(s, i ,j);
-            totalCount = totalCount + oddPalindromeSubstrCount + evenPalindromeSubstrCount;
+            int evenPalSubStringKaCount = expandAroundCenter(s, i, j);
+
+            totalCount += oddPalSubStringKaCount + evenPalSubStringKaCount;
         }
         return totalCount;
     }
